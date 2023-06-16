134ef043adb921158fab1ae918f97123bb016732
Mpamphs-Sougias <136465817+Mpamphs-Sougias@users.noreply.github.com>
2023-06-16 19:44:17 +0300

    Update general-software.sh

--- general-software.sh
+++ general-software.sh
@@ -3,8 +3,23 @@
 #############################
 # software        
 #############################
-sudo apt-get install xorg xinit nano feh  cups build-essential lxappearance pulseaudio alsa-utils pavucontrol pulsemixer rxvt-unicode  pcmanfm -yy
+
+sudo apt-get install xorg xinit nano feh  cups build-essential lxappearance mpv pulseaudio alsa-utils pavucontrol pulsemixer rxvt-unicode  pcmanfm -yy
+
 #############################
 # fonts themes 
-############################
-apt-get install xfonts-terminus 
+#############################
+sudo apt install ttf-mscorefonts-installer -yy
+sudo apt-get install xfonts-terminus -yy 
+sudo apt-get install numix-gtk-theme blackbird-gtk-theme -yy
+
+###########################
+#amd drivers(non-free contrib required)
+###########################
+sudo apt-get firmware-linux-free firmware-linux-nonfree -yy
+
+#########
+#downloader
+#########
+sudo apt install transmission-gtk wget curl -yy
+
