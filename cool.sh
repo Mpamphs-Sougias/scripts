6829ccd5aea227ea56ca526b73a24f29d410f280
Mpamphs-Sougias <136465817+Mpamphs-Sougias@users.noreply.github.com>
2023-06-16 19:33:44 +0300

    Create cool.sh

---
+++ cool.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#############################
+# software        
+#############################
+sudo apt-get install xorg xinit nano feh  cups build-essential lxappearance pulseaudio alsa-utils pavucontrol pulsemixer rxvt-unicode  pcmanfm -yy
+#############################
+# fonts themes 
+############################
+apt-get install xfonts-terminus 
